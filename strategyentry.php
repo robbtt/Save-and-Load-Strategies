c0acd1a6d44613ac47ef3a78b782953a9447a464
robbtt <robbtt@live.com>
2014-02-18 12:39:09 -0600

    Create strategyentry.php

---
+++ strategyentry.php
@@ -0,0 +1,18 @@
+Save-and-Load-Strategies
+========================
+
+//Creates the form data entry for the strategy name and strategy
+
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<form action="strategydatabase.php" method="get">
+Name: <input type="text" id="name" name="name"></br>
+Strategy: <input  style="width: 500px; height: 500px;" type="text" id="strategy" name="strategy"></br>
+
+<input type="submit" name="submit">
+</form>
+
+</body>
+</html>
